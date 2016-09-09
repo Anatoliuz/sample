5390d550c2e594ade1ddf62d6f54cc63156ea198
Anatoliuz <afilinovs@gmail.com>
2016-09-09 17:12:16 +0300

    Num of sent forms

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+'use strict'
 let express = require('express');
 let technologger = require('technologger');
 let parser = require('body-parser');
 let app = express();
-
+let counters = {};
 app.use('/', express.static('public'));
 
-app.use(parser.json());
-app.use(technologger);
 
+app.use(parser.json());
+//app.use(technologger);
+
+//let foo ={};
+//foo.name === undefined
 app.post('/users', (req, res) => {
     console.log(req.body);
-    res.send('100');
+    let email = req.body.email;
+
+    if(!(email in counters)){
+        console.log(email, counters);
+        counters[email] = 0;
+    }
+    ++counters[email];
+    console.log(counters[email]);
+
+    res.send(counters[email] + '');
     // TODO: вернуть количество обращений
 });
 
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+'use strict'
 let assert = require('assert');
 let hello = require('./public/main').hello;
 let plural = require('./public/main').plural;
